c803dee4d6b85c8e23d87f4e11e97033fc9e0177
Stefann Lotter <stevelotter14@gmail.com>
2025-02-04 23:17:53 +0200

    Added ascii art to the selections with appropriate messages

--- Rock Paper Scissors Python CLI Game/game.py
+++ Rock Paper Scissors Python CLI Game/game.py
@@ -1,34 +1,75 @@
 import assets
-from assets import user_options, selections
 import random
 
-print("Welcome to Steve's Rock paper Scissors CLI Game!")
-print("Please choose one of the following numbers [1 - 3] to make your selection.")
-print()
-print("0 : Rock 🪨")
-print("1 : Paper 📃")
-print("2 : Scissors ✂️")
 
-user_selection = input("Your choice: ")
-computer_selection = random.choice(selections)
-print(f"You chose : {user_selection} .")
-print(f"The computer chose \n{computer_selection} ")
-print(user_selection, computer_selection)
+
+
+
+
+
+print("Welcome to the rock paper scissors python CLI game! ")
+computer_selection = random.randint(0, 2)
+
+
+print('\n\nPlease choose one of the following options by typing in its corresponding number :'
+      '\n0 : Rock'
+      '\n1 : Paper'
+      '\n2: Scissors'
+      '')
+
+user_selection = int(input('Your selection : '))
+print(f"The computer chose : {computer_selection}")
 
 if user_selection == computer_selection:
-    print("It's a tie!")
-elif user_selection == "0" and computer_selection == "2":
-    print("You Lose! Paper beats rock")
-elif user_selection == "0" and computer_selection == "3":
-    print("You Win! Rock beats scissors")
-elif user_selection == "2" and computer_selection == "0":
-    print("You Win! Paper beats Rock")
-elif user_selection == "2" and computer_selection == "3":
-    print("You Lose! Scissors beats Paper ")
-elif user_selection == "3" and computer_selection == "0":
-    print("You Lose! Rock beats paper. ")
-elif user_selection == "3" and computer_selection == "2":
-    print("You Win! Scissors beats Paper")
+    print(assets.tie)
+    print("it's a tie!")
+    # Logic conditionals for Rock
+elif user_selection == 0 and computer_selection == 1:
+    print('you chose rock!\n')
+    print(assets.rock)
+    print('The computer chose Paper!\n')
+    print(assets.paper)
+    print("\nYou Lose! Rock gets beaten by Paper. \n")
+
+elif user_selection == 0 and computer_selection == 2:
+    print('you chose rock!\n')
+    print(assets.rock)
+    print('The computer chose Scissors!\n')
+    print(assets.scissors)
+    print("You Win! Rock beats Scissors. ")
+
+    # Logic conditionals for Paper
+elif user_selection == 1 and computer_selection == 0:
+    print('you chose Paper!\n')
+    print(assets.paper)
+    print('The computer chose Rock!\n')
+    print(assets.rock)
+    print("You Win! Paper beats Rock. ")
+
+elif user_selection == 1 and computer_selection == 2:
+    print('you chose Paper!\n')
+    print(assets.paper)
+    print('The computer chose Scissors!\n')
+    print(assets.scissors)
+    print("You Lose! Paper gets beaten by Scissors. ")
+
+    # Logic conditionals for Scissors
+elif user_selection == 2 and computer_selection == 0:
+    print('you chose Scissors!\n')
+    print(assets.scissors)
+    print('The computer chose Rock!\n')
+    print(assets.rock)
+    print("You Lose! Scissors gets beaten by Rock. ")
+
+elif user_selection == 2 and computer_selection == 1:
+    print('you chose Scissors!\n')
+    print(assets.scissors)
+    print('The computer chose Paper!\n')
+    print(assets.paper)
+    print("You Win! Scissors beats Paper. ")
+
+else:
+    print("invalid input")
 
 
 
